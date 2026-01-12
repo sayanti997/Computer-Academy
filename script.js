84d4d10219d535f87b51387b8988e5aa13f435cf
Sayanti Pan <sayantipan2005@gmail.com>
2026-01-12 14:58:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuBtn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
+
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+});
